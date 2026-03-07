16adf8f4128224b0072b8e1d94d52de43effe7c3
bullbearmarket <moneymaker.earnmoney@gmail.com>
2026-03-07 22:14:19 +0530

    Update optionEngine.js

--- engines/optionEngine.js
+++ engines/optionEngine.js
@@ -2,15 +2,26 @@ const axios = require("axios");
 
 let optionCache = {};
 
-async function fetchOptionChain(symbol, atm){
+async function fetchOptionChain(symbol){
 
   try{
 
-    const url = `https://api.upstox.com/v2/option/chain?symbol=${symbol}&strike=${atm}`;
+    let instrument;
+
+    if(symbol === "NIFTY"){
+      instrument = "NSE_INDEX|Nifty%2050";
+    }
+
+    if(symbol === "BANKNIFTY"){
+      instrument = "NSE_INDEX|Nifty%20Bank";
+    }
+
+    const url = `https://api.upstox.com/v2/option/chain?instrument_key=${instrument}`;
 
     const response = await axios.get(url,{
       headers:{
-        Authorization: `Bearer ${process.env.UPSTOX_TOKEN}`
+        Authorization: `Bearer ${process.env.UPSTOX_TOKEN}`,
+        Accept: "application/json"
       }
     });
 
@@ -20,7 +31,7 @@ async function fetchOptionChain(symbol, atm){
 
   }catch(err){
 
-    console.log("Option Chain Error", err.message);
+    console.log("Option Chain Error:", err.response?.data || err.message);
 
   }
 
