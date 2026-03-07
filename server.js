52bedcb16840ddf7d83198a7392e5a2d1b3cae85
bullbearmarket <moneymaker.earnmoney@gmail.com>
2026-03-07 14:09:48 +0530

    Update server.js

--- server.js
+++ server.js
@@ -66,6 +66,7 @@ async function fetchOptionChain() {
     "Accept": "application/json",
     "Accept-Language": "en-US,en;q=0.9",
     "Referer": "https://www.nseindia.com/option-chain"
+    "Connection": "keep-alive",
   }
 });
 
@@ -145,6 +146,7 @@ app.get("/", (req, res) => {
 app.listen(PORT, () => {
   console.log("Server running on port " + PORT);
 });
+
 
 
 
