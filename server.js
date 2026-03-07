bb23f16c4ea32277ce5f3df27073a1088041ac91
bullbearmarket <moneymaker.earnmoney@gmail.com>
2026-03-07 13:37:12 +0530

    Update server.js

--- server.js
+++ server.js
@@ -57,13 +57,18 @@ async function fetchMarket() {
 /* OPTION CHAIN */
 
 async function fetchOptionChain() {
+
   try {
-    const response = await api.get(
+
+    const response = await axios.get(
       "https://www.nseindia.com/api/option-chain-indices?symbol=NIFTY",
       {
         headers: {
           "User-Agent": "Mozilla/5.0",
-          Accept: "application/json"
+          "Accept": "application/json",
+          "Accept-Language": "en-US,en;q=0.9",
+          "Referer": "https://www.nseindia.com/option-chain",
+          "Connection": "keep-alive"
         }
       }
     );
@@ -76,32 +81,46 @@ async function fetchOptionChain() {
     let strikes = {};
 
     records.forEach(item => {
+
       if (
         item.strikePrice >= atm - 250 &&
         item.strikePrice <= atm + 250
       ) {
+
         if (item.CE && item.PE) {
+
           strikes[item.strikePrice] = {
+
             ce: item.CE.lastPrice,
             pe: item.PE.lastPrice,
             ce_oi: item.CE.openInterest,
             pe_oi: item.PE.openInterest
+
           };
+
         }
+
       }
+
     });
 
-    await db.ref("optionchain/nifty").set({
-      atm: atm,
+    const optionData = {
       spot: spot,
+      atm: atm,
       strikes: strikes,
       time: Date.now()
-    });
+    };
 
-    console.log("Option Chain Updated | ATM:", atm);
+    await db.ref("optionchain/nifty").set(optionData);
+
+    console.log("Option chain updated");
+
   } catch (err) {
+
     console.log("Option Chain Error:", err.message);
+
   }
+
 }
 
 /* UPDATE INTERVAL */
@@ -118,4 +137,5 @@ app.get("/", (req, res) => {
 app.listen(PORT, () => {
   console.log("Server running on port " + PORT);
 });
+
 
