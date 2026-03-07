c550c112c8b25c6983041a40b6553a8c3fc8efe9
bullbearmarket <moneymaker.earnmoney@gmail.com>
2026-03-07 13:43:10 +0530

    Update server.js

--- server.js
+++ server.js
@@ -60,18 +60,19 @@ async function fetchOptionChain() {
 
   try {
 
-    const response = await axios.get(
-      "https://www.nseindia.com/api/option-chain-indices?symbol=NIFTY",
-      {
-        headers: {
-          "User-Agent": "Mozilla/5.0",
-          "Accept": "application/json",
-          "Accept-Language": "en-US,en;q=0.9",
-          "Referer": "https://www.nseindia.com/option-chain",
-          "Connection": "keep-alive"
-        }
-      }
-    );
+   const axiosInstance = axios.create({
+  headers: {
+    "User-Agent": "Mozilla/5.0",
+    "Accept": "application/json",
+    "Referer": "https://www.nseindia.com/option-chain"
+  }
+});
+
+await axiosInstance.get("https://www.nseindia.com");
+
+const response = await axiosInstance.get(
+  "https://www.nseindia.com/api/option-chain-indices?symbol=NIFTY"
+);
 
     const records = response.data.records.data;
     const spot = response.data.records.underlyingValue;
@@ -137,5 +138,6 @@ app.get("/", (req, res) => {
 app.listen(PORT, () => {
   console.log("Server running on port " + PORT);
 });
+
 
 
