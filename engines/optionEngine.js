0624e77e26c265e2c49f75eec71d446f6177054e
bullbearmarket <moneymaker.earnmoney@gmail.com>
2026-03-09 14:44:47 +0530

    Update optionEngine.js

--- engines/optionEngine.js
+++ engines/optionEngine.js
@@ -1,38 +1,41 @@
 const axios = require("axios");
 
-let optionCache = {};
+let optionCache = {
+  NIFTY:null,
+  BANKNIFTY:null
+};
 
-async function fetchOptionChain(symbol, expiry){
+async function fetchOptionChain(symbol,expiry){
 
   try{
 
     let instrument;
 
-    if(symbol === "NIFTY"){
-      instrument = "NSE_INDEX|Nifty%2050";
+    if(symbol==="NIFTY"){
+      instrument="NSE_INDEX|Nifty%2050";
     }
 
-    if(symbol === "BANKNIFTY"){
-      instrument = "NSE_INDEX|Nifty%20Bank";
+    if(symbol==="BANKNIFTY"){
+      instrument="NSE_INDEX|Nifty%20Bank";
     }
 
     const url =
     `https://api.upstox.com/v2/option/chain?instrument_key=${instrument}&expiry_date=${expiry}`;
 
-    const response = await axios.get(url,{
+    const res = await axios.get(url,{
       headers:{
         Authorization:`Bearer ${process.env.UPSTOX_TOKEN}`,
         Accept:"application/json"
       }
     });
 
-    optionCache[symbol] = response.data;
+    optionCache[symbol]=res.data;
 
-    console.log(symbol + " option chain updated");
+    console.log(symbol+" option chain updated");
 
   }catch(err){
 
-    console.log("Option Chain Error:", err.response?.data || err.message);
+    console.log("Option Chain Error:",err.response?.data||err.message);
 
   }
 
@@ -42,7 +45,7 @@ function getOptionChain(symbol){
   return optionCache[symbol];
 }
 
-module.exports = {
+module.exports={
   fetchOptionChain,
   getOptionChain
 };
