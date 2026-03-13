8992cae566bcf2f8d0a5db5ac6d16fbe30009c36
bullbearmarket <moneymaker.earnmoney@gmail.com>
2026-03-13 12:54:05 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,10 +1,10 @@
 const admin = require("firebase-admin");
 
-const serviceAccount = require("./serviceAccountKey.json");
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_KEY);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://bullbearmarket7-default-rtdb.asia-southeast1.firebasedatabase.app"
+  databaseURL: process.env.FIREBASE_DB
 });
 
 const db = admin.database();
