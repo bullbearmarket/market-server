f1a2e8d9c558615b12933b26b010b9146c72a514
bullbearmarket <moneymaker.earnmoney@gmail.com>
2026-03-15 12:13:27 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,14 +1,14 @@
 const admin = require("firebase-admin");
 
 if (!admin.apps.length) {
+
   const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_KEY);
 
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
     databaseURL: process.env.FIREBASE_DB
   });
+
 }
 
-const db = admin.database();
-
-module.exports = db;
+module.exports = admin;
